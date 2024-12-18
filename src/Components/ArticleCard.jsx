d9970908ec52af4c4d37dc7356e12e7cc162e07b
AndrewLau97 <andrewlau1997@hotmail.co.uk>
2024-12-18 09:41:40 +0000

    task 7 add/subtract votes

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,9 +1,9 @@
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import { formatDate } from "../utils";
+import { useState } from "react";
+import { updateArticleVotes } from "../api";
 
 function ArticleCard({ articleData }) {
   const {
@@ -15,11 +15,27 @@ function ArticleCard({ articleData }) {
     article_img_url,
     comment_count,
   } = articleData;
+  const [articleVotesAdded, setArticleVotesAdded] = useState(0);
+
   const formattedDate = formatDate(created_at, false);
   const navigate = useNavigate();
   const handleArticleClick = (e) => {
     navigate(`/article/${article_id}`);
   };
+  const handleVotesClick = (e) => {
+      const voteValue=Number(e.target.value)
+      updateArticleVotes(article_id,voteValue).catch((err)=>{
+        console.log("error",err)
+        alert("Please try again later")
+        setArticleVotesAdded((currArticleVotesAdded) => {
+            return currArticleVotesAdded - voteValue;
+          });
+    })
+    setArticleVotesAdded((currArticleVotesAdded) => {
+      return currArticleVotesAdded + voteValue;
+    });
+  };
+
   return (
     <div className="ArticleCard">
       <CardContent onClick={handleArticleClick}>
@@ -34,11 +50,28 @@ function ArticleCard({ articleData }) {
           <br />
         </p>
       </CardContent>
-      <CardMedia sx={{ height: 140 }} image={article_img_url} onClick={handleArticleClick}/>
-      <CardActions className="CardButtons">
-          <button>Votes:{votes}</button>
-          <button>Comments:{comment_count}</button>
-      </CardActions>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={article_img_url}
+        onClick={handleArticleClick}
+        className="ArticleImage"
+      />
+        <p className="VotesText">Votes: {votes + articleVotesAdded}</p>
+      <div className="CardButtons">
+        {votes+articleVotesAdded>0&&
+        <button
+          onClick={handleVotesClick}
+          className="DownVoteButton"
+          value="-1"
+        >
+          Dislike
+        </button>
+        }
+        <button onClick={handleVotesClick} className="UpVoteButton" value="1">
+          Like
+        </button>
+        <button>Comments:{comment_count}</button>
+      </div>
     </div>
   );
 }
